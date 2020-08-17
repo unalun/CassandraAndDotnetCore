356e6dc8d536708af0281bbc97b41569e0055607
unalun <unal.un@hotmail.com>
2020-08-17 21:29:49 +0300

    project files

---
+++ CassandraAndDotnetCore/Program.cs
@@ -0,0 +1,63 @@
+﻿using Cassandra;
+using System;
+using System.Linq;
+
+namespace CassandraAndDotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+
+
+            ISession session = cluster.Connect("phuxingdb");
+
+            var DataList = session.Execute("select * from customer").ToList();
+
+            foreach (var item in DataList) 
+            {
+               Console.WriteLine("Id : {0} FullName : {1} {2} ", item["id"].ToString(), item["name"], item["surname"]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
